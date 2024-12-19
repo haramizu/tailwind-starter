3abdd5e2dac21a441da622642869ff6d7ffa2ace
Shinichi Haramizu <shinichi@haramizu.jp>
2024-12-19 17:30:16 +0900

    add columnsplitter css

--- headapps/tailwind-starter/tailwind.config.js
+++ headapps/tailwind-starter/tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [],
+  safelist: [
+    {
+      pattern: /basis-/,
+      variants: ['sm', 'md', 'lg', 'xl', '2xl'],
+    },
+  ],
   theme: {
     extend: {},
   },
