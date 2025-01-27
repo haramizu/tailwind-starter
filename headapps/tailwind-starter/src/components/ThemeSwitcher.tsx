de05648046d5e33c886b393f9dbed620bb03bbe7
Shinichi Haramizu <shinichi@haramizu.jp>
2025-01-27 13:47:59 +0900

    add style for themeswitcher

--- headapps/tailwind-starter/src/components/ThemeSwitcher.tsx
+++ headapps/tailwind-starter/src/components/ThemeSwitcher.tsx
@@ -1,24 +1,31 @@
+import { ComponentParams, ComponentRendering } from '@sitecore-jss/sitecore-jss-nextjs';
 import { useTheme } from 'next-themes';
-import React, { useEffect } from 'react';
 
-export const Default = (): JSX.Element => {
+interface ComponentProps {
+  rendering: ComponentRendering & { params: ComponentParams };
+  params: ComponentParams;
+}
+
+export const Default = (props: ComponentProps): JSX.Element => {
+  const styles = `${props.params.GridParameters ?? ''} ${props.params.Styles ?? ''}`.trimEnd();
+
   const { theme, setTheme } = useTheme();
 
-  useEffect(() => {}, [theme]);
-
   return (
-    <div>
-      <button onClick={() => setTheme('light')} disabled={theme === 'light'}>
-        Light
-      </button>
-      <span> - </span>
-      <button onClick={() => setTheme('dark')} disabled={theme === 'dark'}>
-        Dark
-      </button>
-      <span> - </span>
-      <button onClick={() => setTheme('system')} disabled={theme === 'system'}>
-        System
-      </button>
+    <div className={`component theme-switcher ${styles}`}>
+      <div className="component-content">
+        <button onClick={() => setTheme('light')} disabled={theme === 'light'}>
+          Light
+        </button>
+        <span> - </span>
+        <button onClick={() => setTheme('dark')} disabled={theme === 'dark'}>
+          Dark
+        </button>
+        <span> - </span>
+        <button onClick={() => setTheme('system')} disabled={theme === 'system'}>
+          System
+        </button>
+      </div>
     </div>
   );
 };
