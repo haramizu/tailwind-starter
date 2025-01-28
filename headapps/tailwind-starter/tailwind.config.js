f0073e4cd49f764a978b6ab183b7681acd5504f2
Shinichi Haramizu <shinichi@haramizu.jp>
2025-01-28 18:16:14 +0900

    update tailwind.config.js

--- headapps/tailwind-starter/tailwind.config.js
+++ headapps/tailwind-starter/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class'],
-  content: ['../src/**/*.{js,ts,jsx,tsx,mdx}'],
+  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   safelist: [
     `flex`,
     {
